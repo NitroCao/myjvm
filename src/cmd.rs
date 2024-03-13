931e46ebc47e5d2c8ab4be9f91171df2569aabf8
Nitro Cao <jaycecao520@gmail.com>
2024-03-13 12:18:42 +0800

    finish chapter 1

---
+++ src/cmd.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// classpath of Java command
+    #[arg(short = 'c', long)]
+    classpath: Option<String>,
+    /// main class to run
+    class: String,
+    /// optional arguments
+    args: Vec<String>,
+}
+
+impl Args {
+    pub fn classpath(&self) -> &Option<String> {
+        &self.classpath
+    }
+
+    pub fn class(&self) -> &String {
+        &self.class
+    }
+
+    pub fn args(&self) -> &Vec<String> {
+        &self.args
+    }
+}
